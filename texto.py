8cc2710c5bb7935f01304b915396715bc62501d2
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-09-28 20:22:53 -0300

    Adicionando "texto.py", que contém o projeto principal, dividido em 2 classes: 1) "class Texto" e 2) "class Cpf". 1): tem métodos para tratamento de informações textuais, como adicionar separadores, sendo um exemplo típico números de telefone; descartar os dados não numéricos de um dado de qualquer tipo; obter uma lista de algarismos a partir de um número; conectar termos semanticamente (com vírgula e a conjunção informada); mesclar duas listas, concatenando cada elemento da 2ª lista entre parenteses ao lado direito de cada elemento da 1ª lista. 2): é uma aplicação específica para lidar com o Cadastro de Pessoa Física (CPF), um documento essencial para transações financeiras no Brasil. Algumas das funções de "class Texto" são chamadas pelas funções de "class Cpf"; atualmente as funções de "Cpf" permitem: a): adicionar a marcação típica do CPF (XXX.XXX.XXX-DV, sendo X cada dígito do do CPF e os 2 últimos dígitos verificadores; b): obter os dígitos verificadores; c): validar o CPF através dos DVs (comparar os DVs obtidos com os DVs contidos no CPF informado).

--- texto.py
+++ texto.py
@@ -1,1 +1,98 @@
-../family_db/texto.py
+class Texto:
+    def adicionar_separador(numero, posicao_do_final=4, separador="-"):
+        numero = str(numero)
+        if len(numero) <= posicao_do_final:
+            return numero + separador
+        else:      
+            texto_separado = ""
+            for i in range(0, len(numero)):
+                texto_separado += numero[i]
+                if i == len(numero) - (posicao_do_final + 1):
+                    texto_separado += separador
+        return texto_separado
+
+    def conectar(lista_de_itens, conjuncao="e"):
+        conjuncao = str(conjuncao)
+        texto = ""
+        for i in range(0, len(lista_de_itens)):
+            texto += str(lista_de_itens[i])
+            if i == len(lista_de_itens) - 1:
+                return texto
+            elif i == len(lista_de_itens) - 2:
+                texto += (" " + conjuncao + " ")
+            else:
+                texto += ", "
+
+    def qual(lista):
+        return Texto.conectar(lista, "ou") + "?"
+
+    def lista_parenteses(lista_1, lista_2):
+        lista_parenteses = []
+        item = ""
+        for i in range(0, len(lista_1)):
+            item += (str(lista_1[i]) + " ("
+            + str(lista_2[i]) + ")")
+            lista_parenteses.append(item)
+            item = ""
+        return lista_parenteses
+    
+    def reter_numeros(texto):
+        tipo = type(texto)
+        if tipo == int or tipo == bool:
+            return texto
+        inteiro = ""
+        for c in str(texto):
+            if 48 <= ord(c) <= 57:
+                inteiro += c
+        if inteiro == "":
+            return texto
+        return int(inteiro)
+    
+    def obter_digitos(numero, limite=False):
+        numero = str(numero)
+        if limite == False:
+            limite = len(numero)
+        lista_digitos = []
+        for i in range(0, limite):
+            lista_digitos.append(int(numero[i]))
+        return lista_digitos
+
+class Cpf:
+    def marcar(cpf):
+        n_cpf = Texto.reter_numeros(cpf)
+        assert type(n_cpf) == int
+        n_cpf = str(n_cpf)
+        if len(n_cpf) != 11:
+            return False
+        n_cpf = Texto.adicionar_separador(n_cpf, 2)
+        n_cpf = Texto.adicionar_separador(n_cpf, 6, ".")
+        return Texto.adicionar_separador(n_cpf, 10, ".")
+  
+    def calcular_dv(digitos_cpf):
+        multiplicador = len(digitos_cpf) + 1
+        calculo_dv = []
+        for i in digitos_cpf:
+            i *= multiplicador
+            calculo_dv.append(i)
+            multiplicador -= 1
+        dv = 0
+        for j in calculo_dv:
+            dv += j
+            dv = dv % 11
+        if dv < 2:
+            return 0
+        return 11 - dv
+    
+    def obter_dvs(cpf):
+        digitos_cpf = Texto.obter_digitos(cpf, 9)
+        primeiro_dv_cpf = Cpf.calcular_dv(digitos_cpf)
+        digitos_cpf.append(primeiro_dv_cpf)
+        segundo_dv_cpf = Cpf.calcular_dv(digitos_cpf)
+        return primeiro_dv_cpf, segundo_dv_cpf
+    
+    def verificar(cpf):
+        cpf = str(cpf)
+        dvs = Cpf.obter_dvs(cpf)
+        if int(cpf[-1]) == dvs[-1] and int(cpf[-2]) == dvs[-2]:
+            return True
+        return False
