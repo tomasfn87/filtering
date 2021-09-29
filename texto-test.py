56f514131d9a4359da639fb464864b71a0fc8245
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-09-29 10:54:23 -0300

    Movidos testes relativos a "class Cpf" para "id-test.py".

--- texto-test.py
+++ texto-test.py
@@ -184,33 +184,3 @@ class TestTexto:
     ])
     def test_T_obter_digitos_completo(self, numero, limite, resultado, T):
         assert T.obter_digitos(numero, limite) == resultado
-
-# Class Cpf
-    @pytest.mark.parametrize("cpf, resultado", [
-        ("11144477799", "111.444.777-99"),
-        ("11144477735", "111.444.777-35"),
-        ("111444777999", False)
-    ])
-    def test_C_marcar(self, cpf, resultado, C):
-        assert C.marcar(cpf) == resultado
-    
-    @pytest.mark.parametrize("digitos_cpf, resultado", [
-        ([1, 1, 1, 4, 4, 4, 7, 7, 7], 3),
-        ([1, 1, 1, 4, 4, 4, 7, 7, 7, 3], 5)
-    ])
-    def test_C_calcular_dv(self, digitos_cpf, resultado, C):
-        assert C.calcular_dv(digitos_cpf) == resultado
-    
-    @pytest.mark.parametrize("cpf, resultado", [
-        (11144477735, (3, 5)),
-        (11144477799, (3, 5)),
-    ])
-    def test_C_obter_dvs(self, cpf, resultado, C):
-        assert C.obter_dvs(cpf) == resultado
-    
-    @pytest.mark.parametrize("cpf, resultado", [
-        (11144477735, True),
-        (11144477799, False)
-    ])
-    def test_C_verificar(self, cpf, resultado, C):
-        assert C.verificar(cpf) == resultado
