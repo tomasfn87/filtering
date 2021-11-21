eed7afae2f54ab3111a93ddfaf589d03c44a77c6
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-11-21 13:25:48 -0300

    Alterando a mensagem de erro quando o arquivo a ser minificado seria sobrescrito para exibir o nome do arquivo a ser renomeado sozinho na última linha, restando ao usuário apenas o trabalho de editar o nome do arquivo original.

--- minJson.sh
+++ minJson.sh
@@ -5,7 +5,8 @@ read -p  "Save minified JSON file: " -e -i "$(echo $JSONFILE)" MINIFIEDJSON;
 while [ $MINIFIEDJSON == $JSONFILE ]
 do
     echo "ERROR: cannot overwrite $JSONFILE.";
-    read -p "Please rename your minified JSON file: " -e -i "$(echo $JSONFILE)" MINIFIEDJSON
+    echo "Please rename your minified JSON file:";
+    read -ei "$(echo $JSONFILE)" MINIFIEDJSON
 done
 
 python3 minifyJson.py $JSONFILE | cat > $MINIFIEDJSON;
