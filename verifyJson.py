33d7002d458bfa8624d139a1eb43c12e3be7ad86
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-11-22 17:15:20 -0300

    Modificado o sistema de retornos de erro para corresponder ao uso comum em bash: 0 (= zero erro) para Ok, e demais casos para casos de erro.

--- verifyJson.py
+++ verifyJson.py
@@ -1,15 +1,28 @@
 import json
 
-def verify(jsonFile):
+def isExtensionJson(file):
+    if len(file) < 6:
+        return False
+    fileExtension = ""
+    for i in range(-5, 0):
+        fileExtension += file[i]
+    if fileExtension != ".json":
+        return False
+    return True
+
+def verify(file):
+    if not isExtensionJson(file):
+        print(1) # Error #1: not a JSON file
+        return
+    jsonFile = file
     with open(jsonFile, "r") as fh:
         json_str = fh.read()
-    
     try:
         json_data = json.loads(json_str)
-        print(1)
+        print(0) #1: Ok, valid JSON
         return
     except:
-        print(0)
+        print(2) # Error #2: invalid JSON data
         return
 
 if __name__ == "__main__":
