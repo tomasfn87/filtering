a194c0949dcb4c1186d26a3df743dd7e5713d9c9
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-11-22 01:55:44 -0300

    O script agora usa "./verifyJson.py" para verificar se o arquivo JSON informado é valido antes de iniciar a minificação.

--- minJson.sh
+++ minJson.sh
@@ -1,14 +1,28 @@
-read -p "Read JSON file: " -e -i "$(echo $HOME)"/ JSONFILE;
+echo "JSON file to be minified:";
+read -ei "$(echo $HOME)"/ JSONFILE; echo;
 
-read -p  "Save minified JSON file: " -e -i "$(echo $JSONFILE)" MINIFIEDJSON;
+echo "Checking JSON file integrity...";
+
+if [ $( python3 ./verifyJson.py $JSONFILE ) == 0 ]
+then
+    echo "ERROR: invalid JSON file. Exiting...";
+    exit 1;
+else
+    echo "DONE.";
+fi;
+
+echo; echo "Save minified JSON file to:";
+read -ei "$(echo $JSONFILE)" MINIFIEDJSON; echo;
 
 while [ $MINIFIEDJSON == $JSONFILE ]
 do
-    echo "ERROR: cannot overwrite $JSONFILE.";
-    echo "Please rename your minified JSON file:";
-    read -ei "$(echo $JSONFILE)" MINIFIEDJSON
-done
+    echo "ERROR: cannot overwrite '$JSONFILE'.";
+    echo; echo "Please rename the minified file or save it to another folder:";
+    read -ei "$(echo $JSONFILE)" MINIFIEDJSON;
+done;
 
-python3 minifyJson.py $JSONFILE | cat > $MINIFIEDJSON;
+python3 ./minifyJson.py $JSONFILE | cat > $MINIFIEDJSON;
+echo; echo "Minified JSON saved to: ";
+echo "'$MINIFIEDJSON'.";
 
-echo Minified Json saved at $MINIFIEDJSON.
+exit 0
