3056252eb4402f56fac4bd384e25748c4d413b6d
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-11-23 01:16:24 -0300

    Adicionadas cores no texto do script: 1) verde para "OK" e "DONE" e vermelho para "ERROR".

--- minJson.sh
+++ minJson.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+OK () {
+    gawk -F ":" '
+    BEGIN {
+    printf "%s", "\033[1;32m" "OK" "\033[0m"
+    } '
+}
+
+DONE () {
+    gawk -F ":" '
+    BEGIN {
+    printf "%s", "\033[1;32m" "DONE" "\033[0m"
+    } '
+}
+
+ERROR () {
+    gawk -F ":" '
+    BEGIN {
+    printf "%s", "\033[31m" "ERROR" "\033[0m"
+    } '
+}
+
 verifyJson () {
     echo $(python3 $HOME/filtering/verifyJson.py $1)
 }
@@ -7,16 +29,16 @@ openJson () {
 
     if [ $(verifyJson $1) == 1 ];
     then
-        echo "ERROR: not a JSON file.";
+        echo "$(ERROR): not a JSON file.";
         echo; echo "Please choose a JSON file ('file.json'):";
         return 1
     elif [ $(verifyJson $JSONFILE) == 2 ];
     then 
-        echo "ERROR: invalid JSON data.";
+        echo "$(ERROR): invalid JSON data.";
         echo; echo "Please choose a valid JSON file:";
         return 2
     else
-        echo "JSON validation: OK"
+        echo "JSON validation: $(OK)";
         return 0
     fi;
 }
@@ -35,13 +57,13 @@ read -ei "$(echo $JSONFILE)" MINIFIEDJSON;
 
 while [ $MINIFIEDJSON == $JSONFILE ]
 do
-    echo "ERROR: cannot overwrite source file '$JSONFILE'.";
+    echo "$(ERROR): cannot overwrite source file '$JSONFILE'.";
     echo; echo "Please rename the minified file or save it to another folder:";
     read -ei "$(echo $MINIFIEDJSON)" MINIFIEDJSON;
 done;
 
 echo "Minifying JSON file..."
 python3 $HOME/filtering/minifyJson.py $JSONFILE | cat > $MINIFIEDJSON;
-echo "JSON minification: DONE";
+echo "JSON minification: $(DONE)";
 echo; echo "File saved to $MINIFIEDJSON";
 exit 0;
