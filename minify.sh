af9c399f830ef416e8367a78d8a5ca456d2dbb72
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-11-25 10:01:55 -0300

    Adicionado "minify.sh", que tem por objetivo a minificação de qualquer arquivo que se beneficie da remoção de espaços vazios fora de strings e de line escapes.

---
+++ minify.sh
@@ -0,0 +1,24 @@
+toGreen () { gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;32m" text "\033[0m" }'
+}
+
+toRed () { gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;31m" text "\033[0m" }'
+}
+
+toBlue () { gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;34m" text "\033[0m" }'
+}
+
+if [ $1 == $2 ]
+then
+    echo "$(toRed ERROR): cannot overwrite source file $(toBlue $2)";
+    echo "Please rename the minified file or save it to another folder";
+    exit 1
+fi;
+
+echo "Minifying file..."
+python3 $HOME/filtering/minify.py $1 | cat > $2;
+echo "File minification: $(toGreen DONE)";
+echo "File saved to $(toGreen $2)";
+exit 0;
