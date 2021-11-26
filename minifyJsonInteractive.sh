fe480b0816c3cb90def88d21d1e1ca9cb65f3592
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-11-26 13:53:06 -0300

    Renomeado "minifyJson.sh" para "minifyJsonInteractive.sh", pois trata-se de uma ferramenta interativa para minificar um arquivo JSON, que ajuda o usuário a selecionar o arquivo de origem e a salvar o arquivo contendo o resultado corretamente: 1): impede que sejam selecionados arquivos de origem com extensão diferente de ".json"; 2): impede que sejam selecionados arquivos de origem com conteúdo JSON inválido; 3): impede que o arquivo contendo o resultado sobrescreva o arquivo de origem - nos três casos, se o arquivo for inválido o usuário irá receber a mensagem de erro apropriada e terá então a oportunidade de alterar o arquivo, continuando a execução do programa.

---
+++ minifyJsonInteractive.sh
@@ -0,0 +1,74 @@
+toGreen () { gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;32m" text "\033[0m" }' 
+}
+
+toRed () { gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;31m" text "\033[0m" }'
+}
+
+toBlue () { gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;34m" text "\033[0m" }'
+}
+
+verifyJson () {
+    echo $(python3 $HOME/filtering/verifyJson.py $1)
+}
+
+openJson () {
+    echo "Checking JSON file integrity...";
+
+    if [ $(verifyJson $1) == 1 ];
+    then
+        echo "$(toRed ERROR): not a JSON file";
+        echo; echo "Please choose a JSON file ('file.json'):";
+        return 1
+    elif [ $(verifyJson $JSONFILE) == 2 ];
+    then 
+        echo "$(toRed ERROR): invalid JSON data";
+        echo; echo "Please choose a valid JSON file:";
+        return 2
+    else
+        echo "JSON validation: $(toGreen OK)";
+        return 0
+    fi;
+}
+
+echo "Choose JSON file to minify:";
+read -ei "$(echo $HOME)"/ JSONFILE;
+openJson $JSONFILE
+while [ $? != 0 ];
+do
+    read -ei "$(echo $JSONFILE)" JSONFILE;
+    openJson $JSONFILE
+done;
+
+echo; echo "Save minified JSON file to:";
+read -ei "$(echo $JSONFILE)" MINIFIEDJSON;
+
+while [ $MINIFIEDJSON == $JSONFILE ]
+do
+    echo "$(toRed ERROR): cannot overwrite source file $(toBlue $JSONFILE)";
+    echo "Please rename the minified file or save it to another folder:";
+    read -ei "$(echo $MINIFIEDJSON)" MINIFIEDJSON;
+done;
+
+while [ $(verifyJson $MINIFIEDJSON) == 1 ];
+do
+    echo "$(toRed ERROR): minified JSON file extension must be '.json'";
+    echo "Please change the file extension to '.json':";
+    read -ei "$(echo $MINIFIEDJSON)" MINIFIEDJSON;
+done;
+
+echo "Minifying JSON file..."
+python3 $HOME/filtering/minify.py $JSONFILE | cat > $MINIFIEDJSON;
+echo "JSON minification: $(toGreen DONE)";
+echo; echo "File saved to $(toBlue $MINIFIEDJSON)";
+
+echo "Checking '$MINIFIEDJSON'..."
+if [ $(verifyJson $MINIFIEDJSON) != 0 ];
+then
+    echo "$(toRed ERROR): minified JSON validation failed";
+else
+    echo "Minified JSON validation: $(toGreen OK)";
+fi;
+exit 0;
