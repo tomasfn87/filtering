c6de31d5d1ca7fc864bec1679d074a1d82ffbec0
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-12-07 20:02:58 -0300

    1): adicionada verificação: agora o script verifica se o arquivo de origem existe. 2): alterada a interface: agora os estágios do processo vão sendo completados em formato de checklist.

--- verifyJson.sh
+++ verifyJson.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-paintGreen () {
-    gawk -v text=$1 'BEGIN {
-        printf "%s", "\033[1;32m" text "\033[0m"
-    }'
+toGreen () {
+  gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;32m" text "\033[0m"
+  }'
 }
 
-paintRed () {
-    gawk -v text=$1 'BEGIN {
-        printf "%s", "\033[1;31m" text "\033[0m"
-    }'
+toRed () {
+  gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;31m" text "\033[0m"
+  }'
 }
 
 verifyJson () {
-    echo $(python3 $HOME/filtering/verifyJson.py $1)
+  echo $(python3 $HOME/filtering/verifyJson.py $1)
 }
 
+echo -n " * Checking if source file exists...    "
+if [ -f $1 ];
+then
+  echo "[$(toGreen OK)]";
+else
+  echo "$(toRed ERROR): file not found"
+  exit 3;
+fi;
+
+echo -n " * Checking if source filed is JSON...  " 
 if [ $(verifyJson $1) == 0 ];
 then
-    echo "JSON validation: $(paintGreen OK)";
-    exit 0;
+  echo "[$(toGreen OK)]";
+  exit 0;
 elif [ $(verifyJson $1) == 1 ];
 then
-    echo "$(paintRed ERROR): not a JSON file";
-    exit 1;
+  echo "$(toRed ERROR): not a JSON file";
+  exit 1;
 else
-    echo "$(paintRed ERROR): invalid JSON data";
-    exit 2;
+  echo "$(toRed ERROR): invalid JSON data";
+  exit 2;
 fi;
