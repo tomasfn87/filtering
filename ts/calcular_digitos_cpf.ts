a26540adfd7c4b3a83adb0aa2a0b4db92d98e0f2
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-12-18 17:02:18 -0300

    Adicionada verificação do comprimento do número de CPF: 1): se o número de CPF recebido não tiver ao menos 9 dígitos, será impressa uma mensagem de erro, e será retornada uma lista vazia. 2): se o número tiver mais do que 9 dígitos, será impressa uma mensagem de aviso e os dígitos verificadores serão calculados usando apenas os 9 primeiros dígitos do número de CPF recebido.

--- ts/calcular_digitos_cpf.ts
+++ ts/calcular_digitos_cpf.ts
@@ -1,29 +1,32 @@
 type numero_cpf = string | number
-type digitos_cpf = number[]
+type lista_digitos_cpf = number[]
 type digito_verificador_cpf = number
 type digitos_verificadores_cpf = number[]
 
-// const meuCpf:numero_cpf = 111444777
+// const Cpf:numero_cpf = 111444777
 const Cpf:numero_cpf = process.argv[2]
 
-const obter_lista_digitos = (numero_cpf: numero_cpf):digitos_cpf => {
+const obter_lista_digitos = (numero_cpf: numero_cpf):lista_digitos_cpf => {
     if (typeof numero_cpf === 'number') {
         numero_cpf = numero_cpf.toString()
     }
-    let digitos_cpf:digitos_cpf = []
+    let lista_digitos_cpf:lista_digitos_cpf = []
     for (let i = 0; i < numero_cpf.length; i++) {
-        digitos_cpf.push(parseInt(numero_cpf[i]))
+        lista_digitos_cpf.push(parseInt(numero_cpf[i]))
     }
-
-    return digitos_cpf
+    if (lista_digitos_cpf.length > 9) {
+        console.warn(
+            `Foi recebido um número de CPF com ${lista_digitos_cpf.length} dígitos: só os 9 primeiros serão utilizados`
+        )
+        return lista_digitos_cpf.slice(0, 9)
+    }
+    return lista_digitos_cpf
 }
 
-let lista_digitos_cpf:digitos_cpf = obter_lista_digitos(Cpf)
-
-const calcular_dv = (digitos_cpf:digitos_cpf):digito_verificador_cpf => {
-    let multiplicador:number = digitos_cpf.length + 1
+const calcular_dv = (lista_digitos_cpf:lista_digitos_cpf):digito_verificador_cpf => {
+    let multiplicador:number = lista_digitos_cpf.length + 1
     let calculo_dv = []
-    for (let i of digitos_cpf) {
+    for (let i of lista_digitos_cpf) {
         i *= multiplicador
         calculo_dv.push(i)
         multiplicador -= 1
@@ -39,11 +42,23 @@ const calcular_dv = (digitos_cpf:digitos_cpf):digito_verificador_cpf => {
     return 11 - dv
 }
 
-const obter_dvs = (numero_cpf:numero_cpf):digitos_verificadores_cpf => {
-    let digitos_cpf:digitos_cpf = obter_lista_digitos(numero_cpf)
-    let primeiro_dv_cpf:digito_verificador_cpf = calcular_dv(digitos_cpf)
-    digitos_cpf.push(primeiro_dv_cpf)
-    return [primeiro_dv_cpf, calcular_dv(digitos_cpf)]
+const obter_dvs = (Cpf:numero_cpf):digitos_verificadores_cpf => {
+    let digitos_cpf:lista_digitos_cpf = obter_lista_digitos(Cpf)
+    if (digitos_cpf.length < 9) {
+        console.error(
+            `Foi recebido um número de CPF com apenas ${digitos_cpf.length} dígitos: o CPF deve ter ao menos 9 dígitos`
+        )
+        return []
+    }
+    let cpf_informado = `${digitos_cpf.slice(0,3).join("")}`
+    cpf_informado += `.${digitos_cpf.slice(3,6).join("")}`
+    cpf_informado += `.${digitos_cpf.slice(6,9).join("")}`
+    console.log(`CPF informado: ${cpf_informado}`)
+    let dv_1:digito_verificador_cpf = calcular_dv(digitos_cpf)
+    let dv_2:digito_verificador_cpf = digitos_cpf.push(dv_1)
+    dv_2 = calcular_dv(digitos_cpf)
+    console.log(`CPF completo: ${cpf_informado}-${dv_1}${dv_2}`)
+    return [dv_1, dv_2]
 }
 
 console.log(obter_dvs(Cpf))
