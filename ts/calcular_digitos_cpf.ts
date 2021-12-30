bdbb0153ba65848cac72e53017ff3860e30f8155
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-12-30 13:58:39 -0300

    1): agora o arquivo "calcular_digitos_cpf.ts" não contém mais as linhas de código que permitem que sejam passados argumentos via CLI (interface de linha de comando), apenas as funções para tal. Para calcular os dígitos de CPF via linha de comando, deve ser usado o arquivo "CLI_calcular_digitos_cpf.ts". 2): as funções "obter_lista_digitos" e "obter_dvs" agora recebem um segundo parâmetro, "info", que têm por valor padrão "n": se receber "a", "A", "i" ou "I", serão exibidas informações adicionais. No caso de "calcular_digitos", não existem diferenças entre as informações exibidas pelas opções "a" ou "i".

--- ts/calcular_digitos_cpf.ts
+++ ts/calcular_digitos_cpf.ts
@@ -1,53 +1,70 @@
-type numero_cpf = string | number
-type lista_digitos_cpf = number[]
-type digito_verificador_cpf = number
-type digitos_verificadores_cpf = number[]
+type numero_cpf = string | number;
+type lista_digitos_cpf = number[];
+type digito_verificador_cpf = number;
+type digitos_verificadores_cpf = number[];
 
-const obter_lista_digitos = (numero_cpf: numero_cpf):lista_digitos_cpf => {
-/* A função "obter_lista_digitos" recebe como argumento uma string ou
+const obter_lista_digitos = (
+    numero_cpf: numero_cpf, info:string='n'
+):lista_digitos_cpf => {
+/*  A função "obter_lista_digitos" recebe como argumento uma string ou
 um número; se o argumento for um número, será transformado em string;
 se o tamanho do número for maior que 9, será impressa uma mensagem de
 aviso, informado que apenas os 9 primeiros dígitos serão utilizados
 para calcular os 2 dígitos verificadores. A função retorna uma lista
-cujos elementos são os dígitos do número de CPF recebido. */
-    if (typeof numero_cpf === 'number') {
+cujos elementos são os dígitos do número de CPF recebido.
+    O parâmetro "info" tem por valor padrão 'n', e pode receber 'i' ou
+'a' (ou 'I' ou 'A'): ambas imprimirão informações adicionais. Nesse
+caso, não há diferença entre 'i' e 'a'.
+*/
+    if (typeof numero_cpf !== 'string') {
         numero_cpf = numero_cpf.toString()
-    }
-    let lista_digitos_cpf:lista_digitos_cpf = []
+    };
+
+    let lista_digitos_cpf:lista_digitos_cpf = [];
+
     for (let i = 0; i < numero_cpf.length; i++) {
         lista_digitos_cpf.push(parseInt(numero_cpf[i]))
-    }
+    };
+
     if (lista_digitos_cpf.length > 9) {
-        console.warn(
-            `Foi recebido um número de CPF com ${lista_digitos_cpf.length} dígitos: só os 9 primeiros serão utilizados`
-        )
-        return lista_digitos_cpf.slice(0, 9)
+        if (info.toLowerCase() === 'i' || info.toLowerCase() === 'a') {
+            console.warn(
+                `Foi recebido um número de CPF com ${lista_digitos_cpf.length} dígitos: só os 9 primeiros serão utilizados`
+            );
+        }
+        return lista_digitos_cpf.slice(0, 9);
     }
-    return lista_digitos_cpf
-}
+    return lista_digitos_cpf;
+};
 
 const calcular_dv = (lista_digitos_cpf:lista_digitos_cpf):digito_verificador_cpf => {
 /* A função "calcular_dv" executa o algoritmo de cálculo dos dígitos
 verificadores de um número de CPF. */
-    let multiplicador:number = lista_digitos_cpf.length + 1
-    let calculo_dv = []
-    for (let i of lista_digitos_cpf) {
-        i *= multiplicador
-        calculo_dv.push(i)
-        multiplicador -= 1
-    }
-    let dv = 0
-    for (let j of calculo_dv) {
-        dv += j
-        dv %= 11
-    }
-    if (dv < 2) {
-        return 0
-    }
-    return 11 - dv
-}
+    let multiplicador:number = lista_digitos_cpf.length + 1;
+    let calculo_dv = [];
 
-const obter_dvs = (Cpf:numero_cpf):digitos_verificadores_cpf => {
+    for (let i of lista_digitos_cpf) {
+        i *= multiplicador;
+        calculo_dv.push(i);
+        multiplicador -= 1;
+    }
+
+    let dv = 0;
+
+    for (let j of calculo_dv) {
+        dv += j;
+        dv %= 11;
+    }
+
+    if (dv < 2) {
+        return 0;
+    }
+    return 11 - dv;
+};
+
+const obter_dvs = (
+    Cpf:numero_cpf, info:string='n'
+):digitos_verificadores_cpf => {
 /* A função "obter_dvs" recebe uma lista de dígitos, e verifica se essa
 lista possui ao menos 9 elementos: em caso negativo, imprime uma
 mensagem de erro e retorna uma lista vazia; em caso positivo, a função
@@ -59,29 +76,28 @@ padrão: XXX.XXX.XXX-DD, onde "X" são os dígitos do CPF e "D" são os
 dígitos verificadores. A função retornará uma lista com os dígitos
 verificadores.
 */
-    let digitos_cpf:lista_digitos_cpf = obter_lista_digitos(Cpf)
+    let digitos_cpf:lista_digitos_cpf = obter_lista_digitos(Cpf, info);
     if (digitos_cpf.length < 9) {
-        console.error(
-            `Foi recebido um número de CPF com apenas ${digitos_cpf.length} dígitos: o CPF deve ter ao menos 9 dígitos`
-        )
-        return []
+        if (info.toLowerCase() === 'i' || info.toLowerCase() === 'a') {
+            console.error(
+                `Foi recebido um número de CPF com apenas ${digitos_cpf.length} dígitos: o CPF deve ter ao menos 9 dígitos`
+            );
+        }
+        return [];
     }
-    let cpf_informado = `${digitos_cpf.slice(0,3).join("")}`
-    cpf_informado += `.${digitos_cpf.slice(3,6).join("")}`
-    cpf_informado += `.${digitos_cpf.slice(6,9).join("")}`
-    console.log(`CPF informado: ${cpf_informado}`)
-    let dv_1:digito_verificador_cpf = calcular_dv(digitos_cpf)
-    let dv_2:digito_verificador_cpf = digitos_cpf.push(dv_1)
-    dv_2 = calcular_dv(digitos_cpf)
-    console.log(`CPF completo: ${cpf_informado}-${dv_1}${dv_2}`)
-    return [dv_1, dv_2]
-}
+    let cpf_informado = `${digitos_cpf.slice(0,3).join("")}`;
+    cpf_informado += `.${digitos_cpf.slice(3,6).join("")}`;
+    cpf_informado += `.${digitos_cpf.slice(6,9).join("")}`;
+    if (info.toLowerCase() === 'i' || info.toLowerCase() === 'a') {
+        console.log(`CPF informado: ${cpf_informado}`);
+    };
+    let dv_1:digito_verificador_cpf = calcular_dv(digitos_cpf);
+    let dv_2:digito_verificador_cpf = digitos_cpf.push(dv_1);
+    dv_2 = calcular_dv(digitos_cpf);
+    if (info.toLowerCase() === 'i' || info.toLowerCase() === 'a') {
+        console.log(`CPF completo: ${cpf_informado}-${dv_1}${dv_2}`)
+    };
+    return [dv_1, dv_2];
+};
 
-// const Cpf:numero_cpf = 111444777
-const Cpf:numero_cpf = process.argv[2]
-/* "Cpf" recebe um argumento através da linha de comando, para que o
-número de CPF cujos dígitos serão calculados possa ser informado
-diretamente através da CLI.
-Exemplo: ts-node calcular_digitos_cpf.ts 111444777 */
-
-console.log(obter_dvs(Cpf))
+export default obter_dvs;
