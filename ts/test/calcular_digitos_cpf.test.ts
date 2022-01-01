fe25e9aa3457350d041ec904221ca4d718d696a8
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-01 16:37:59 -0300

    Adicionado testes: 1): para números de CPF com caracteres não numéricos (exemplo: "111.444.777-35") para a função "calcular_digitos_cpf.ts.obter_lista_digitos"; 2): para números de CPF com menos de 9 caracteres para a função "calcular_digitos_cpf.ts.obter_dvs".

--- ts/test/calcular_digitos_cpf.test.ts
+++ ts/test/calcular_digitos_cpf.test.ts
@@ -13,6 +13,11 @@ describe('Calcular dígitos CPF', function() {
         expect(result).to.eql([1,1,1,4,4,4,7,7,7]); // needed to use chai .to.eql to compare arrays #1
     });
 
+    it('../calcular_digitos_cpf.ts.obter_lista_digitos - "111.444.777-35" -> [1,1,1,4,4,4,7,7,7]', function() {
+        let result = obter_lista_digitos('111.444.777-35');
+        expect(result).to.eql([1,1,1,4,4,4,7,7,7]); // needed to use chai .to.eql to compare arrays #1
+    });
+
     it('../calcular_digitos_cpf.ts.obter_lista_digitos - 111444777 -> [1,1,1,4,4,4,7,7,7]', function() {
         let result = obter_lista_digitos(111444777);
         expect(result).to.eql([1,1,1,4,4,4,7,7,7]); // needed to use chai .to.eql to compare arrays #2
@@ -26,5 +31,10 @@ describe('Calcular dígitos CPF', function() {
     it('../calcular_digitos_cpf.ts.obter_dvs - "11144477746" -> [3,5]', function() {
         let result = obter_dvs('11144477746');
         expect(result).to.eql([3, 5]); // needed to use chai .to.eql to compare arrays #3
+    });
+    
+    it('../calcular_digitos_cpf.ts.obter_dvs - "11144477" -> []', function() {
+        let result = obter_dvs('11144477');
+        expect(result).to.eql([]); // needed to use chai .to.eql to compare arrays #3
     });
 });
