eac12daab07d82cb45374aab4adeaee963561d16
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-01 16:21:28 -0300

    Alterando as descrições e cada teste em "ts/test/calcular_digitos_cpf.test.ts" e "ts/test/verificar_digitos_cpf.test.ts", para que indiquem: 1): o arquivo e 2): a função testados, 3): o argumento passado e 4): o resultado esperado (antes não eram exibidos  nem argumento passado para a função e nem o resultado esperado como retorno, havia uma descrição breve da função, que foi removida).

--- ts/test/calcular_digitos_cpf.test.ts
+++ ts/test/calcular_digitos_cpf.test.ts
@@ -3,23 +3,28 @@ import { calcular_dv, obter_lista_digitos } from "../calcular_digitos_cpf";
 import { expect } from "chai";
 
 describe('Calcular dígitos CPF', function() {
-    it('../calcular_digitos_cpf.ts.calcular_dv - Calcular dígito verificador a partir de um número de CPF', function() {
+    it('../calcular_digitos_cpf.ts.calcular_dv - [1,1,1,4,4,4,7,7,7] -> 3', function() {
         let result = calcular_dv([1,1,1,4,4,4,7,7,7]);
         expect(result).to.eql(3); // and it also works just fine here, so I'll be testing it #4
     });
 
-    it('../calcular_digitos_cpf.ts.obter_lista_digitos - Obter Lista de dígitos de um número de CPF', function() {
+    it('../calcular_digitos_cpf.ts.obter_lista_digitos - "111444777" -> [1,1,1,4,4,4,7,7,7]', function() {
         let result = obter_lista_digitos('111444777');
         expect(result).to.eql([1,1,1,4,4,4,7,7,7]); // needed to use chai .to.eql to compare arrays #1
     });
 
-    it('../calcular_digitos_cpf.ts.obter_lista_digitos - Obter Lista de dígitos de um número de CPF', function() {
+    it('../calcular_digitos_cpf.ts.obter_lista_digitos - 111444777 -> [1,1,1,4,4,4,7,7,7]', function() {
         let result = obter_lista_digitos(111444777);
         expect(result).to.eql([1,1,1,4,4,4,7,7,7]); // needed to use chai .to.eql to compare arrays #2
     });
 
-    it('../calcular_digitos_cpf.ts.obter_dvs - Obter os 2 dígitos verificadores a partir de um número de CPF', function() {
+    it('../calcular_digitos_cpf.ts.obter_dvs - "111444777" -> [3,5]', function() {
         let result = obter_dvs('111444777');
+        expect(result).to.eql([3, 5]); // needed to use chai .to.eql to compare arrays #3
+    });
+
+    it('../calcular_digitos_cpf.ts.obter_dvs - "11144477746" -> [3,5]', function() {
+        let result = obter_dvs('11144477746');
         expect(result).to.eql([3, 5]); // needed to use chai .to.eql to compare arrays #3
     });
 });
--- ts/test/verificar_digitos_cpf.test.ts
+++ ts/test/verificar_digitos_cpf.test.ts
@@ -3,32 +3,32 @@ import { comparar_dvs } from "../verificar_digitos_cpf";
 import { expect } from "chai";
 
 describe('Verificar dígitos CPF', function() {
-    it('../verificar_digitos_cpf.ts.comparar_dvs - Comparar dígitos verificadores', function() {
+    it('../verificar_digitos_cpf.ts.comparar_dvs - [[3,5],[3,5]] -> true', function() {
         let result = comparar_dvs([3,5], [3,5]);
         expect(result).equal(true);
     });
 
-    it('../verificar_digitos_cpf.ts.comparar_dvs - Comparar dígitos verificadores', function() {
+    it('../verificar_digitos_cpf.ts.comparar_dvs - [[3,6],[3,5]] -> false', function() {
         let result = comparar_dvs([3,6], [3,5]);
         expect(result).equal(false);
     });
 
-    it('../verificar_digitos_cpf.ts.verificar_cpf - Verificar dígitos verificadores de um CPF', function() {
+    it('../verificar_digitos_cpf.ts.verificar_cpf - "11144477735" -> true', function() {
         let result = verificar_cpf('11144477735');
         expect(result).equal(true);
     });
 
-    it('../verificar_digitos_cpf.ts.verificar_cpf - Verificar dígitos verificadores de um CPF', function() {
+    it('../verificar_digitos_cpf.ts.verificar_cpf - 11144477735 -> true', function() {
         let result = verificar_cpf(11144477735);
         expect(result).equal(true);
     });
 
-    it('../verificar_digitos_cpf.ts.verificar_cpf - Verificar dígitos verificadores de um CPF', function() {
+    it('../verificar_digitos_cpf.ts.verificar_cpf - "11144477736" -> false', function() {
         let result = verificar_cpf('11144477736');
         expect(result).equal(false);
     });
 
-    it('../verificar_digitos_cpf.ts.verificar_cpf - Verificar dígitos verificadores de um CPF', function() {
+    it('../verificar_digitos_cpf.ts.verificar_cpf - 11144477736 -> false', function() {
         let result = verificar_cpf(11144477736);
         expect(result).equal(false);
     });
