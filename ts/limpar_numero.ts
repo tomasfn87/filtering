2b20b45cb269a8f884ff2b531c1fa9a484bddc98
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-01 12:34:02 -0300

    Adicionando arquivo "limpar_numero.ts", que contém a função "limpar_numero", que extrai uma string da primeira camada de um objeto iterável, de um number ou de uma string propriamente dita, e verifica se os caracteres são numéricos.

---
+++ ts/limpar_numero.ts
@@ -0,0 +1,34 @@
+type numero = number | string | object;
+type string_numerica = string;
+
+const limpar_numero = (numero:numero):string_numerica => {
+  let numero_em_limpeza:string_numerica = "";
+  let numero_limpo:string_numerica = "";
+
+  if (typeof numero === 'object' && Array.isArray(numero) && numero.length != 0) {
+    for (let i of numero) {
+        if (typeof i === 'string') {
+          numero_em_limpeza += i;
+        } else if (typeof i === 'number') {
+            numero_em_limpeza += i.toString();
+        };
+    };
+  } else if (typeof numero === 'number') {
+    numero_em_limpeza = numero.toString();
+  } else if (typeof numero === 'string') {
+    if (numero.length === 0) {
+        return numero_limpo;
+    };
+    numero_em_limpeza = numero;
+  };
+
+  for (let c of numero_em_limpeza) {
+    if (c.charCodeAt(0) >= 48 && c.charCodeAt(0) <= 57) {
+      numero_limpo += c
+    }
+  };
+
+  return numero_limpo;
+}
+
+export default limpar_numero;
