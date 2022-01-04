4bba5b55602a5f03cda8304a098627b0ad5fb2e2
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-04 14:23:19 -0300

    Alterando a importação de "texto": ao invés de importar o módulo completo, agora é importada a única classe usada, "Texto". Em decorrência, foram alteradas as chamadas da classe "Texto": de "T.Texto" para apenas "T".

--- id.py
+++ id.py
@@ -1,16 +1,16 @@
-import texto as T
+from texto import Texto as T
 
 class Cpf:
     def marcar(cpf):
-        n_cpf = T.Texto.reter_numeros(cpf)
+        n_cpf = T.reter_numeros(cpf)
         assert type(n_cpf) == int
         n_cpf = str(n_cpf)
         if len(n_cpf) != 11:
             return False
-        n_cpf = T.Texto.adicionar_separador(n_cpf, 2)
-        n_cpf = T.Texto.adicionar_separador(n_cpf, 6, ".")
-        return T.Texto.adicionar_separador(n_cpf, 10, ".")
-  
+        n_cpf = T.adicionar_separador(n_cpf, 2)
+        n_cpf = T.adicionar_separador(n_cpf, 6, ".")
+        return T.adicionar_separador(n_cpf, 10, ".")
+
     def calcular_dv(digitos_cpf):
         multiplicador = len(digitos_cpf) + 1
         calculo_dv = []
@@ -27,7 +27,8 @@ class Cpf:
         return 11 - dv
     
     def obter_dvs(cpf):
-        digitos_cpf = T.Texto.obter_lista_digitos(cpf, 9)
+        cpf = T.reter_numeros(cpf, True)
+        digitos_cpf = T.obter_lista_digitos(cpf, 9)
         primeiro_dv_cpf = Cpf.calcular_dv(digitos_cpf)
         digitos_cpf.append(primeiro_dv_cpf)
         return primeiro_dv_cpf, Cpf.calcular_dv(digitos_cpf)
@@ -37,3 +38,4 @@ class Cpf:
         if int(cpf[-1]) == dvs[-1] and int(cpf[-2]) == dvs[-2]:
             return True
         return False
+    
